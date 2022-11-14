7fc7961ba3352a206c0c566e522082bea26617b7
jerry <jerry19971228@gmail.com>
2022-11-14 15:28:54 +0800

    將科系、畢業國中、畢業狀態改成下拉選單

--- add.php
+++ add.php
@@ -58,19 +58,39 @@ $pdo=new PDO($dsn,'root','');
         </tr>
         <tr>
             <td>graduate_at</td>
-            <td></td>
+            <td>
+                <select name="graduate_at">
+                    <?php
+                    $sql="SELECT * FROM `graduate_school`";
+                    $grads=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($grads as $grad){
+                        echo "<option value='{$grad['id']}'>{$grad['county']}{$grad['name']}</option>";
+                    }
+                    ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td>status_code</td>
-            <td></td>
+            <td>
+                <select name="status_code">
+                    <?php
+                    $sql="SELECT * FROM `status`";
+                    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($rows as $row){
+                        echo "<option value='{$row['id']}'>{$row['status']}</option>";
+                    }
+                    ?>
+                </select>
+            </td>
         </tr>
         <tr>
             <td>班級</td>
-            <td></td>
+            <td><input type="text" name="classes"></td>
         </tr>
         <tr>
             <td>座號</td>
-            <td></td>
+            <td><input type="number" name="seat_num"></td>
         </tr>
     </table>
     <input type="submit" value="確認新增">
