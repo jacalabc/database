e8659bd9418b4ed64d59117139bc7706c8434848
jerry <jerry19971228@gmail.com>
2022-11-18 16:06:50 +0800

    首頁增加分班級顯示功能

--- index.php
+++ index.php
@@ -3,8 +3,25 @@
 $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
 $pdo = new PDO($dsn, 'root', '');
 
+if(isset($_GET['code'])){
+$sql="SELECT `students`.`id` as 'id',
+             `students`.`school_num` as '學號',
+             `students`.`name` as '姓名',
+             `students`.`birthday` as '生日',
+             `students`.`graduate_at` as '畢業國中'
+      FROM `class_student`,`students`
+      WHERE `class_student`.`school_num` = `students`.`school_num` &&
+            `class_student`.`class_code` = '{$_GET['code']}'";
+}else{
 //建立撈取學生資料的語法，限制只撈取前20筆
-$sql = "SELECT * FROM `students` limit 20";
+$sql = "SELECT `students`.`id` as 'id',
+               `students`.`school_num` as '學號',
+               `students`.`name` as '姓名',
+               `students`.`birthday` as '生日',
+               `students`.`graduate_at` as '畢業國中'
+        FROM `students` LIMIT 20";
+}
+
 
 //執行SQL語法，並從資料庫取回全部符合的資料，加上PDO::FETCH_ASSOC表示只需回傳帶有欄位名的資料
 $rows = $pdo->query($sql)->fetchALL(PDO::FETCH_ASSOC);
@@ -29,19 +46,31 @@ $rows = $pdo->query($sql)->fetchALL(PDO::FETCH_ASSOC);
     </nav>
 
     <nav>
+        <ul class="class-list">
+        <?php
+        $sql = "SELECT `id`,`code`,`name` FROM `classes`";
+        $classes = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($classes as $class) {
+            echo "<li><a href='?code={$class['code']}'>{$class['name']}</a></li>";
+        }
+        ?>
+        </ul>
+    </nav>
+
+    <nav>
         <!--根據status來顯示回應-->
         <?php
-        if(isset($_GET['status'])){
-            switch($_GET['status']){
+        if (isset($_GET['status'])) {
+            switch ($_GET['status']) {
                 case 'add_success':
                     echo "<span style='color:green'>新增學生成功</span>";
-                break;
+                    break;
                 case 'add_fail';
                     echo "<span style='color:red'>新增學生有誤</span>";
-                break;
+                    break;
                 case 'edit_error':
                     echo "<span style='color:red'>無法編輯，請洽管理員，或正確操作</span>";
-                break;
+                    break;
             }
         }
         ?>
@@ -60,20 +89,20 @@ $rows = $pdo->query($sql)->fetchALL(PDO::FETCH_ASSOC);
         <?php
         //使用迴圈來顯示每一位學生的資料
         foreach ($rows as $row) {
-            $age = round((strtotime('now') - strtotime($row['birthday'])) / (60 * 60 * 24 * 365), 1);
+            $age = round((strtotime('now') - strtotime($row['生日'])) / (60 * 60 * 24 * 365), 1);
 
             echo "<tr>";
-            echo "<td>{$row['school_num']}</td>";
-            echo "<td>{$row['name']}</td>";
-            echo "<td>{$row['birthday']}</td>";
-            echo "<td>{$row['graduate_at']}</td>";
+            echo "<td>{$row['學號']}</td>";
+            echo "<td>{$row['姓名']}</td>";
+            echo "<td>{$row['生日']}</td>";
+            echo "<td>{$row['畢業國中']}</td>";
             echo "<td>{$age}</td>";
             echo "<td>";
             //加上連結將頁面導向edit.php，同時以GET方式將學生資料的id傳遞到edit.php
             echo "<a href='edit.php?id={$row['id']}'>編輯</a>";
             //加上連結將頁面導向del.php，同時以GET方式將學生資料的id傳遞到del.php
             // echo "<a href='del.php?id={$row['id']}'>刪除</a>";
-            echo "<a href='del.php?id={$row['id']}'>刪除</a>";
+            echo "<a href='./api/del_stident.php?id={$row['id']}'>刪除</a>";
             echo "</td>";
             echo "</tr>";
         }
