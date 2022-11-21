a5ec8525b8b9d353dc94a57203eee223cec25cbe
jerry <jerry19971228@gmail.com>
2022-11-21 14:51:32 +0800

    建立教室註冊表單頁面，及後端接收註冊資料的reg_user.php檔案

--- reg.php
+++ reg.php
@@ -8,5 +8,15 @@
 </head>
 <body>
 <h1>教師註冊</h1>
+<form action="./api/reg_user.php" method="post">
+    <div><lable for="">帳號 : <input type="text" name="acc"></lable></div>
+    <div><lable for="">密碼 : <input type="password" name="pw"></lable></div>
+    <div><lable for="">信箱 : <input type="text" name="email"></lable></div>
+    <div><lable for="">姓名 : <input type="text" name="name"></lable></div>
+    <div>
+        <lable for=""><input type="submit" value="註冊"></lable>
+        <lable for=""><input type="reset" value="重製"></lable>
+    </div>
+</form>
 </body>
 </html>
