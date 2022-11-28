cccdb5ad3b16db7436edd0c2d595d68ff51fa493
jerry <jerry19971228@gmail.com>
2022-11-28 15:28:43 +0800

    調整新聞列表的顯示內容，前後台不同

---
+++ api/news_edit.php
@@ -0,0 +1,15 @@
+<?php
+include "../db/base.php";
+
+$sql="UPDATE `news`
+      SET `subject`='{$_POST['subject']}',
+          `content`='{$_POST['content']}',
+          `type`='{$_POST['type']}',
+          `top`='{$_POST['top']}',
+          `readed`='{$_POST['readed']}'
+      WHERE `id`='{$_POST['id']}'";
+
+      $pdo->exec($sql);
+
+header("location:../admin_center.php?do=news");
+?>
