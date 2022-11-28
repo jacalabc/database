870290bc9a1494d817f0bbdf9c191004358efd59
jerry <jerry19971228@gmail.com>
2022-11-28 14:29:38 +0800

    完成編輯消息的表單製作

--- front/main.php
+++ front/main.php
@@ -1,5 +1,18 @@
-<div class="main">
+<h3>最新消息</h3>
+<hr>
+<ul class="list-group">
 
-<h1 class='text-center'>最新消息</h1>
+<?php
+$all_news="SELECT * FROM `news` ";
+$rows=$pdo->query($all_news)->fetchAll();
 
-</div>
+foreach($rows as $row){
+    echo "<li class='list-group-item list-group-item-action d-flex'>";
+    echo "<a href='index.php?do=news_detail&id={$row['id']}'>";
+    echo $row['subject'];
+    echo "</li>";
+}
+
+?>
+
+</ul>
