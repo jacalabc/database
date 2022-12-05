5ca8b977bc2c2cbfe4141048f3f20494eb51ddd9
jerry <jerry19971228@gmail.com>
2022-12-05 10:44:01 +0800

    使用迴圈來完成選項新增功能，並倒回調查列表頁面

--- api/survey_add.php
+++ api/survey_add.php
@@ -5,6 +5,8 @@ include_once "../db/base.php";
 // $type=1;
 // $vote=0;
 // $active=0;
+// dd($_POST['subject']);
+// dd($_POST['opt']);
 // 建立資料陣列
 $subject=['subject'=>$_POST['subject'],
           'type'=>1,
@@ -14,6 +16,20 @@ $subject=['subject'=>$_POST['subject'],
 // 使用insert函式，寫入資料
 insert('survey_subject',$subject);
 
-// insert('survey_options');
+$subject_id=find('survey_subject',['subject'=>$_POST['subject']])['id'];
+
+if(isset($_POST['opt'])){
+    foreach($_POST['opt'] as $option){
+        if($option!=''){
+             $tmp=['opt'=>$option,
+                   'subject_id'=>$subject_id,
+                   'vote'=>0];
+        dd($tmp);
+        insert('survey_options',$tmp);
+        }                     
+    }
+}
+
+header("location:../admin_center.php?do=survey");
 
 ?>
