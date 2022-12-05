6f00bb6cf0b1face02a22f7875f82acfdf43368b
jerry <jerry19971228@gmail.com>
2022-12-05 13:26:05 +0800

    完成刪除調查主題工作

--- api/survey_del.php
+++ api/survey_del.php
@@ -1,7 +1,16 @@
 <?php
 include_once "../db/base.php";
 
+// 調查主題的id
+$id=$_GET['id'];
 
+// 關聯的選項的id
+// $options=all("survey_options",['subject_id'=>$id]);
 
+del("survey_subject",$id);
+
+del("survey_options",['subject_id'=>$id]);
+
+header("location:../admin_center.php?do=survey");
 
 ?>
